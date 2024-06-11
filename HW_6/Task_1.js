e4e39b8b156caf4956b328aa76a69cd3ecb41c73
Alexey_Bodyagin <azartnyi7@gmail.com>
2024-06-12 00:35:13 +0300

    This is HW_6 only task 1 and 2

---
+++ HW_6/Task_1.js
@@ -0,0 +1,28 @@
+// 1. Создайте функцию getEmployeeInfo()
+// 2. В функции создайте массив имен сотрудников 5шт (Имена придумать самим)
+// 3. В функции создайте массив зарплат сотрудников 5 шт(цифры придумать самим)
+// 4. Функция должна принимать 1 аргумент - имя сотрудника
+// 5. Функция должна возвращать новый массив, где первый элемент - имя сотрудника, второй - его зарплата
+// 6. Для поиска ответа функции нужно найти индекс сотрудника в массиве имен. Зарплату взять с ТЕМ ЖЕ индексом что и имя
+// 7. Для возврата из функции создайте массив, методом .push поместите в него имя и зарплату, и через return верните созданный массив
+// 8. Если такое имя сотрудника в массиве не найдется - вернуть null
+
+function getEmployeeInfo(employeeName) {
+    employeeNames = ['Ann', 'Mary', 'Antony', 'Alex', 'Boris'];
+    employeeSalary = [300, 300, 450, 450, 550];
+
+    const index = employeeNames.indexOf(employeeName);
+
+    if (index === -1) {
+        return null;
+    }
+
+    const nameAndSalary = [];
+    nameAndSalary.push(employeeNames[index]);
+    nameAndSalary.push(employeeSalary[index]);
+    return nameAndSalary;
+}
+
+console.log(getEmployeeInfo('Ann'));
+console.log(getEmployeeInfo('Alex'));
+console.log(getEmployeeInfo('John'));
